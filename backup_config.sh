ff9db1ec83d471444341fdd12ef1835e0fc18415
Shoma daf FUKUDA <fkshom@gmail.com>
2020-05-30 11:47:55 +0900

    Update backup_config.sh

--- backup_config.sh
+++ backup_config.sh
@@ -1,17 +1,31 @@
 #!/bin/vbash
-source /opt/vyatta/etc/functions/script-template
+
+function usage() {
+  echo "USAGE: $0 [-p]"
+  echo "-p: push after commit"
+  exit
+}
+
+PUSH=0
+while getopts ph OPT; do
+  case $OPT in
+    "p" ) PUSH=1 ;;
+    "h" ) usage ;;
+  esac
+done
+
+#source /opt/vyatta/etc/functions/script-template
 
 #Environment
 SCRIPTDIR=$(cd $(dirname $0); pwd)
 SRCDIR=/config
-BACKUPDIR=./
+DSTDIR=./
 HOSTNAME=`hostname`
 now=`date "+%Y/%m/%d %H:%M:%S"`
 GITOPT="-c user.name='vyos' -c user.email='vyos@example.com'"
 
 #Backup Git
-#cd $SCRIPTDIR
-cd $BACKUPDIR
+cd $DSTDIR
 
 if git show-ref --quiet refs/remote/origin/$HOSTNAME; then
   git checkout $HOSTNAME
@@ -19,9 +33,13 @@ else
   git checkout -B $HOSTNAME
 fi
 
-rsync -a --delete --exclude=archive/ --exclude=vyos-migrate.log $SRCDIR/ $BACKUPDIR/config/
+rsync -a --delete --exclude=archive/ --exclude=vyos-migrate.log $SRCDIR/ $DSTDIR/config/
 
 git add .
 git $GITOPT commit -m "$HOSTNAME backup config $now"
 
-#git push -u origin $HOSTNAME
+if [ $PUSH -eq 1 ]; then
+  git push -u origin $HOSTNAME
+fi
+
+echo "Backup completed"
