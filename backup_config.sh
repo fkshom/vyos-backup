2b8bb4e7c12ba12cd4b26173b9119eda02e72074
Shoma daf FUKUDA <fkshom@gmail.com>
2020-06-03 23:45:31 +0900

    Update backup_config.sh

--- backup_config.sh
+++ backup_config.sh
@@ -10,6 +10,7 @@ PUSH=0
 while getopts ph OPT; do
   case $OPT in
     "p" ) PUSH=1 ;;
+    "b" ) BRANCHSTYLE=1 ;;
     "h" ) usage ;;
   esac
 done
@@ -26,6 +27,8 @@ REPOSITORYDIR=${REPOSITORYDIR:-/config/.backup}
 HOSTNAME=${HOSTNAME:-`hostname`}
 GIT_USERNAME=${GIT_USERNAME:-vyos}
 GIT_USEREMAIL=${GIT_USEREMAIL:-vyos@example.com}
+STORE_STYLE=${STORE_STYLE:-DIR}
+BRANCH_NAME=${BRANCH_NAME:-master}
 
 now=`date "+%Y/%m/%d %H:%M:%S"`
 GITOPT="-c user.name='$GIT_USERNAME' -c user.email='GIT_USEREMAIL'"
@@ -39,19 +42,36 @@ if ! git rev-parse --git-dir; then
   git $GITOPT commit --allow-empty -m "initial blank commit"
 fi
 
-if git show-ref --quiet refs/remotes/origin/$HOSTNAME; then
-  git checkout $HOSTNAME
+
+if [ "$STORE_STYLE" == "BRANCH" ]; then
+  if git show-ref --quiet refs/remotes/origin/$HOSTNAME; then
+    git checkout $HOSTNAME
+  else
+    git checkout -B $HOSTNAME
+  fi
+
+  rsync -a --delete --exclude=$REPOSITORYDIR --exclude=$SCRIPTDIR --exclude=archive/ --exclude=vyos-migrate.log $SRCDIR $REPOSITORYDIR/
+
+  git add .
+  git $GITOPT commit -m "$HOSTNAME backup config $now"
+  
+  if [ $PUSH -eq 1 ]; then
+    git push -u origin $HOSTNAME
+  fi
 else
-  git checkout -B $HOSTNAME
+  if [ ! -d "$HOSTNAME" ]; then
+    mkdir "$HOSTNAME"
+  fi
+  git checkout master
+  rsync -a --delete --exclude=$REPOSITORYDIR --exclude=$SCRIPTDIR --exclude=archive/ --exclude=vyos-migrate.log $SRCDIR $REPOSITORYDIR/$HOSTNAME/
+
+  git add .
+  git $GITOPT commit -m "$HOSTNAME backup config $now"
+
+  if [ $PUSH -eq 1 ]; then
+    git push -u origin master
+  fi
 fi
 
-rsync -a --delete --exclude=$REPOSITORYDIR --exclude=$SCRIPTDIR --exclude=archive/ --exclude=vyos-migrate.log $SRCDIR $REPOSITORYDIR/config
-
-git add .
-git $GITOPT commit -m "$HOSTNAME backup config $now"
-
-if [ $PUSH -eq 1 ]; then
-  git push -u origin $HOSTNAME
-fi
 
 echo "Backup completed"
